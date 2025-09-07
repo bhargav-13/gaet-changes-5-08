e5bee4755d1f91fb5934cb3dee094da5000698d7
Yatri-patel <yatripatel02@gmail.com>
2025-09-07 15:26:28 +0530

    footer and advschoolslider decades changes

--- src/include/ContactApi.jsx
+++ src/include/ContactApi.jsx
@@ -9,7 +9,7 @@ function ContactApi() {
     if (error) return <div className="error-message">Error: {error.message}</div>;
     if (!settings) return null;
 
-    const { facebook, instagram, linked_in, email_id, twitter, youtube,map } = settings?.settings || {};
+    const { facebook, instagram, linked_in, email_id, twitter, youtube, map } = settings?.settings || {};
 
     const socialLinks = [
         { url: facebook, icon: '/images/facebook.svg', name: 'Facebook' },
@@ -17,7 +17,11 @@ function ContactApi() {
         { url: linked_in, icon: '/images/linkdin.svg', name: 'LinkedIn' },
         { url: twitter, icon: '/images/xicon.svg', name: 'Twitter' },
         { url: youtube, icon: '/images/youtube.svg', name: 'YouTube' },
-        { url: map, icon: '/images/MapIcon.png', name: 'Map' }
+        {
+            url: "https://www.google.com/maps/search/?api=1&query=Yashodham+High+School+Building,+Yashodham,+Goregaon(East),+Mumbai+-+400063",
+            icon: '/images/MapIcon.png',
+            name: 'Map'
+        }
     ];
 
     return (
