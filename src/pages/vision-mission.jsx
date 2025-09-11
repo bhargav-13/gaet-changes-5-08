10aa1d1250e2330c8217fb2f6897ab12ff4eda8f
vedantshah46 <vedantshah022@gmail.com>
2025-09-11 23:20:18 +0530

    Our Core Values BUtton Added

--- src/pages/vision-mission.jsx
+++ src/pages/vision-mission.jsx
@@ -11,17 +11,19 @@ import { useApi } from "../hooks/useApi";
 import "./VisionMission.css";
 import { Helmet } from "react-helmet-async";
 import Footer from "../include/Footer";
-import visionOne from '../assets/vissionOne.png';
-import visionTwo from '../assets/vissionTwo.png';
-import realLearning from '../assets/RealLearning.png';
-import rightEnv from '../assets/RightEnv.png';
-import growth from '../assets/HonestGrowth.png';
-import people from '../assets/People.png';
+import visionOne from "../assets/vissionOne.png";
+import visionTwo from "../assets/vissionTwo.png";
+import realLearning from "../assets/RealLearning.png";
+import rightEnv from "../assets/RightEnv.png";
+import growth from "../assets/HonestGrowth.png";
+import people from "../assets/People.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-import cardBg1 from '../assets/card-bg-1.png';
-import cardBg4 from '../assets/card-bg-2.png'; 
-import cardBg3 from '../assets/card-bg-3.png'; 
-import cardBg2 from '../assets/card-bg-4.png'; 
+import cardBg1 from "../assets/card-bg-1.png";
+import cardBg4 from "../assets/card-bg-2.png";
+import cardBg3 from "../assets/card-bg-3.png";
+import cardBg2 from "../assets/card-bg-4.png";
 
 function VisionMissionPage() {
   useEffect(() => {
@@ -46,64 +48,76 @@ function VisionMissionPage() {
       item.addEventListener("click", handleItemClick)
     );
 
-        // Cleanup to avoid memory leaks
-        return () => {
-            listItems.forEach((item) => item.removeEventListener('click', handleItemClick));
-        };
-    }, [data]);
+    // Cleanup to avoid memory leaks
+    return () => {
+      listItems.forEach((item) =>
+        item.removeEventListener("click", handleItemClick)
+      );
+    };
+  }, [data]);
 
-    if (loading) return <div><Loader /></div>;
-    if (error) return <div className="error-message">Error: {error.message}</div>;
-    if (!data) return null;
-
-    const sections = [
-        {
-            title: "Vision",
-            image: visionOne,
-            description: "To create a generation of thoughtful learners who are curious, capable and confident to grow to become passionate, purposeful and compassionate to make a meaningful difference in the world."
-        },
-        {
-            title: "Mission",
-            image: visionTwo,
-            description: "To create a place where every child is seen, heard, and supported with true learning experiences, strong relationships and a safe environment, grow at their own pace."
-        }
-    ];
-
-    const coreValues = [
-        {
-            title: "Real Learning",
-            icon: realLearning,
-            backgroundImage: cardBg1
-        },
-        {
-            title: "Right Environment",
-            icon: rightEnv,
-            backgroundImage: cardBg2
-        },
-        {
-            title: "Honest Growth",
-            icon: growth,
-            backgroundImage: cardBg3
-        },
-        {
-            title: "People Before Policies",
-            icon: people,
-            backgroundImage: cardBg4
-        }
-    ];
+  if (loading)
     return (
-        <>
-            <Helmet>
-                <meta name="description" content={data?.seo?.meta_description || "GAET"} />
-                <meta name="keywords" content={data?.seo?.meta_keywords || "GAET"} />
-            </Helmet>
+      <div>
+        <Loader />
+      </div>
+    );
+  if (error) return <div className="error-message">Error: {error.message}</div>;
+  if (!data) return null;
 
-            <InsideTopBanner
-                pageTitle={"Our Vision & Mission"}
-                pageBreadcrumb="Vision & Mission"
-                BackgrondBack={data.top_section?.back_image}
-                CircleFront={data.top_section?.front_image}
-            />
+  const sections = [
+    {
+      title: "Vision",
+      image: visionOne,
+      description:
+        "To create a generation of thoughtful learners who are curious, capable and confident to grow to become passionate, purposeful and compassionate to make a meaningful difference in the world.",
+    },
+    {
+      title: "Mission",
+      image: visionTwo,
+      description:
+        "To create a place where every child is seen, heard, and supported with true learning experiences, strong relationships and a safe environment, grow at their own pace.",
+    },
+  ];
+
+  const coreValues = [
+    {
+      title: "Real Learning",
+      icon: realLearning,
+      backgroundImage: cardBg1,
+    },
+    {
+      title: "Right Environment",
+      icon: rightEnv,
+      backgroundImage: cardBg2,
+    },
+    {
+      title: "Honest Growth",
+      icon: growth,
+      backgroundImage: cardBg3,
+    },
+    {
+      title: "People Before Policies",
+      icon: people,
+      backgroundImage: cardBg4,
+    },
+  ];
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content={data?.seo?.meta_description || "GAET"}
+        />
+        <meta name="keywords" content={data?.seo?.meta_keywords || "GAET"} />
+      </Helmet>
+
+      <InsideTopBanner
+        pageTitle={"Our Vision & Mission"}
+        pageBreadcrumb="Vision & Mission"
+        BackgrondBack={data.top_section?.back_image}
+        CircleFront={data.top_section?.front_image}
+      />
 
       <div className="breadcrumb-area">
         <Breadcrumb>
@@ -112,48 +126,78 @@ function VisionMissionPage() {
         </Breadcrumb>
       </div>
 
-            <div className="inner-content-wapper">
-                {/* Core Values Section */}
-                <div className="core-values-wrapper">
-                    <div className="title-section">
-                        <h1 className="main-title">Our Core Values</h1>
-                    </div>
+      <div className="inner-content-wapper">
+        {/* Core Values Section */}
+        <div className="core-values-wrapper">
+          <div className="title-section">
+            <h1 className="main-title">Our Core Values</h1>
+          </div>
 
-                    <div className="values-cards-container">
-                        <div className="values-cards">
-                            {coreValues.map((value, index) => (
-                                <div 
-                                    key={index} 
-                                    className="value-card"
-                                    style={{
-                                        backgroundImage: `url(${value.backgroundImage})`
-                                    }}
-                                >
-                                    <div className="card-content">
-                                        <h3 className="card-title">
-                                            {value.title.includes("Before") ? (
-                                                <>
-                                                    People Before <br />
-                                                    Policies
-                                                </>
-                                            ) : (
-                                                value.title.split(" ").map((word, i) => (
-                                                    <span key={i}>
-                                                        {word}
-                                                        {i < value.title.split(" ").length - 1 ? <br /> : ""}
-                                                    </span>
-                                                ))
-                                            )}
-                                        </h3>
-                                        <div className="card-icon">
-                                            <img src={value.icon} alt={value.title} />
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+          <div className="values-cards-container">
+            <div className="values-cards">
+              {coreValues.map((value, index) => (
+                <div
+                  key={index}
+                  className={`value-card ${
+                    index === activeindex ? "active" : "hidden"
+                  }`}
+                  style={{
+                    backgroundImage: `url(${value.backgroundImage})`,
+                  }}
+                >
+                  <div className="card-content">
+                    <h3 className="card-title">
+                      {value.title.includes("Before") ? (
+                        <>
+                          People Before <br />
+                          Policies
+                        </>
+                      ) : (
+                        value.title.split(" ").map((word, i) => (
+                          <span key={i}>
+                            {word}
+                            {i < value.title.split(" ").length - 1 ? (
+                              <br />
+                            ) : (
+                              ""
+                            )}
+                          </span>
+                        ))
+                      )}
+                    </h3>
+                    <div className="card-icon">
+                      <img src={value.icon} alt={value.title} />
                     </div>
+                  </div>
                 </div>
+              ))}
+            </div>
+
+            {/* Navigation */}
+            <div className="custom-nav-icons">
+              <button
+                className="custom-prev pe-0 px-0"
+                onClick={() =>
+                  setActiveindex((prev) =>
+                    prev === 0 ? coreValues.length - 1 : prev - 1
+                  )
+                }
+              >
+                <FontAwesomeIcon icon={faAngleLeft} />
+              </button>
+              <button
+                className="custom-next"
+                onClick={() =>
+                  setActiveindex((prev) =>
+                    prev === coreValues.length - 1 ? 0 : prev + 1
+                  )
+                }
+              >
+                <FontAwesomeIcon icon={faAngleRight} />
+              </button>
+            </div>
+          </div>
+        </div>
 
         <section>
           <div className="vision-mission-wrapper">
